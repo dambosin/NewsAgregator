aca654fe36509a3c7735e9de784de8ae191ea223
dambosin <dambosin@gmail.com>
2023-04-14 14:40:18 +0300

    ArticleCardPartial

---
+++ NewsAgregator/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NewsAgregator.Abstractions.Services;
+
+namespace NewsAgregator.Mvc.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly ICommentService _commentService;
+
+        public CommentController(
+            IMapper mapper, 
+            ICommentService commentService)
+        {
+            _mapper = mapper;
+            _commentService = commentService;
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
--- NewsAgregator/Models/Articles/ArticleModel.cs
+++ NewsAgregator/Models/Articles/ArticleModel.cs
@@ -5,5 +5,6 @@
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string ShortDescription { get; set; }
+        public int LikesCount { get; set; }
     }
 }
